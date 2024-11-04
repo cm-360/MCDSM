05946614720602c9de2eecba2b132000624c5b23
CM360 <31625347+cm-360@users.noreply.github.com>
2024-11-04 13:16:52 -0500

    Fix console broker models import

--- api/app/models/__init__.py
+++ api/app/models/__init__.py
@@ -2,6 +2,6 @@ from .base import Serializable
 
 from .network.network_model import Network
 from .server.server_model import Server
-from .server.server_model import ConsoleBroker
+from .server.console_broker import ConsoleBroker
 
 __all__ = ['Serializable', 'Network', 'Server', 'ConsoleBroker']
